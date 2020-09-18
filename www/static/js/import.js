025cd8e61d9823704487025950e06a9c9c6722ae
siw36 <rhh.klussmann@gmail.com>
2020-09-18 12:13:43 +0200

    updated import.js to new clean templating method

--- www/static/js/import.js
+++ www/static/js/import.js
@@ -28,45 +28,36 @@ function loadTable() {
       var tbody = document.getElementsByTagName("tbody")[0].innerHTML = "";
       if ('content' in document.createElement('template')) {
         $.each(templatesAvialable, function(i, item) {
-          var t = document.querySelector('#import_item_template'),
+          var t = document.querySelector('#import_item_template');
+          var tc = document.importNode(t.content, true);
           // Setting all requeired elements
-          import_id = t.content.querySelector('#import_id');
-          import_name = t.content.querySelector('#import_name');
-          import_description = t.content.querySelector('#import_description');
-          import_button = t.content.querySelector('#import_button');
-          import_button_delete = t.content.querySelector('#import_button_delete');
-          import_form = t.content.querySelector('form');
-          import_form_id = t.content.querySelector('#import_form_id');
-          import_button_icon = t.content.querySelector('#import_button_icon')
+          tc.querySelector('#import_id').textContent = item.id;
+          tc.querySelector('#import_name').textContent = item.name;
+          tc.querySelector('#import_description').textContent = item.description;
+          tc.querySelector('#import_button').setAttribute('form', item.id);
+          tc.querySelector('#import_button_delete').setAttribute('data-template_id', item.id);
+          tc.querySelector('form').id = item.id;
+          tc.querySelector('#import_form_id').setAttribute('value', item.id);
 
           if (templatesImported != undefined) {
             if (templatesImported.filter(function(e) { return e.id == item.id; }).length > 0) {
-              import_button_delete.disabled = false;
-              import_button_delete.hidden = false;
-              import_button_delete.setAttribute ('onclick', 'deleteItem(this)');
-              import_button.className = "btn btn-warning";
-              import_button.setAttribute('title', "Re-import");
-              import_button_icon.className = "fas fa-redo-alt";
+              tc.querySelector('#import_button_delete').disabled = false;
+              tc.querySelector('#import_button_delete').hidden = false;
+              tc.querySelector('#import_button_delete').setAttribute ('onclick', 'deleteItem(this)');
+              tc.querySelector('#import_button').className = "btn btn-warning";
+              tc.querySelector('#import_button').setAttribute('title', "Re-import");
+              tc.querySelector('#import_button_icon').className = "fas fa-redo-alt";
             } else {
-              import_button_delete.disabled = true;
-              import_button_delete.hidden = true;
-              import_button.className = "btn btn-primary";
-              import_button.setAttribute('title', "Import");
-              import_button_icon.className = "fas fa-file-import";
+              tc.querySelector('#import_button_delete').disabled = true;
+              tc.querySelector('#import_button_delete').hidden = true;
+              tc.querySelector('#import_button').className = "btn btn-primary";
+              tc.querySelector('#import_button').setAttribute('title', "Import");
+              tc.querySelector('#import_button_icon').className = "fas fa-file-import";
             }
           }
-          import_id.textContent = item.id;
-          import_name.textContent = item.name;
-          import_description.textContent = item.description;
-          import_button.setAttribute('form', item.id);
-          import_button_delete.setAttribute('data-template_id', item.id);
-
-          import_form.id = item.id;
-          import_form_id.setAttribute('value', item.id);
 
           var tb = document.getElementsByTagName('tbody');
-          var clone = document.importNode(t.content, true);
-          tb[0].appendChild(clone);
+          tb[0].appendChild(tc);
         });
       } else {
         alert("HTML5 templating does not work with your browser")
