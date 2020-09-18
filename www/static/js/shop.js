867465111410b789abdb0dc1d720165dd2aeb300
siw36 <rhh.klussmann@gmail.com>
2020-09-18 12:28:40 +0200

    updated shop.js to new clean templating method

--- www/static/js/shop.js
+++ www/static/js/shop.js
@@ -19,25 +19,21 @@ function loadShopItems() {
     $(function() {
       if ('content' in document.createElement('template')) {
         $.each(response, function(i, item) {
-          var t = document.querySelector('#shop_card_template'),
+          var t = document.querySelector('#shop_card_template');
+          var tc = document.importNode(t.content, true);
           // Setting all requeired elements
-          card_icon = t.content.querySelector('#card_icon');
-          card_name = t.content.querySelector('#card_name');
-          card_description = t.content.querySelector('#card_description');
-          template_id = t.content.querySelector('#template_id');
+          tc.querySelector('#card_name').textContent = item.name;
+          tc.querySelector('#card_description').textContent = item.description;
+          tc.querySelector('#template_id').value = item.id;
 
           if (item.icon != "") {
-            card_icon.setAttribute('src', item.icon);
+            tc.querySelector('#card_icon').setAttribute('src', item.icon);
           } else {
-            card_icon.setAttribute('src', '/static/logo.png');
+            tc.querySelector('#card_icon').setAttribute('src', '/static/logo.png');
           }
-          card_name.textContent = item.name;
-          card_description.textContent = item.description;
-          template_id.value = item.id;
 
           var shop = $('#shop')
-          var clone = document.importNode(t.content, true);
-          shop[0].appendChild(clone);
+          shop[0].appendChild(tc);
         });
       } else {
         alert("HTML5 templating does not work with your browser");
